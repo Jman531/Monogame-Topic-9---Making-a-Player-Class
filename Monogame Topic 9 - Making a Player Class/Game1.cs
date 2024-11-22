b84d328f39f8ad4466b33bc333cf4581b29f1a70
Jack Gallagher <339690067@GoTVDSB.ca>
2024-11-22 09:38:11 -0500

    Class ended

--- Monogame Topic 9 - Making a Player Class/Game1.cs
+++ Monogame Topic 9 - Making a Player Class/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Monogame_Topic_9___Making_a_Player_Class
 {
@@ -11,11 +13,18 @@ namespace Monogame_Topic_9___Making_a_Player_Class
 
         Player amoeba;
 
+        Rectangle window;
+
         KeyboardState keyboardState;
 
         Texture2D amoebaTexture;
         Texture2D wallTexture;
         Texture2D foodTexture;
+
+        List<Rectangle> barriers;
+        List<Rectangle> food;
+
+        Random generator = new Random();
 
         public Game1()
         {
@@ -27,6 +36,20 @@ namespace Monogame_Topic_9___Making_a_Player_Class
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 500);
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.ApplyChanges();
+
+            barriers = new List<Rectangle>();
+            barriers.Add(new Rectangle(100, 100, 10, 200));
+            barriers.Add(new Rectangle(400, 400, 100, 10));
+
+            food = new List<Rectangle>();
+
+            for (int i = 0; i < 10; i++)
+                food.Add(new Rectangle(generator.Next(0, 790), generator.Next(0, 490), 10, 10));
+
 
             base.Initialize();
             amoeba = new Player(amoebaTexture, 10, 10);
@@ -59,8 +82,33 @@ namespace Monogame_Topic_9___Making_a_Player_Class
             }
             else if (keyboardState.IsKeyDown(Keys.A))
             {
+                amoeba.HSpeed = -3;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
                 amoeba.VSpeed = -3;
             }
+            else if (keyboardState.IsKeyDown(Keys.S))
+            {
+                amoeba.VSpeed = 3;
+            }
+
+            amoeba.Update();
+
+            amoeba.Offscreen(window);
+
+            foreach (Rectangle barrier in barriers)
+                if (amoeba.Collide(barrier))
+                    amoeba.UndoMove();
+
+            for (int i = 0; i < food.Count; i++)
+                if (amoeba.Collide(food[i]))
+                {
+                    amoeba.Grow();
+                    food.RemoveAt(i);
+                    i--;
+                }
 
             base.Update(gameTime);
         }
@@ -70,6 +118,15 @@ namespace Monogame_Topic_9___Making_a_Player_Class
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            foreach (Rectangle barrier in barriers)
+                _spriteBatch.Draw(wallTexture, barrier, Color.White);
+            foreach (Rectangle bit in food)
+                _spriteBatch.Draw(foodTexture, bit, Color.White);
+            amoeba.Draw(_spriteBatch);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- Monogame Topic 9 - Making a Player Class/Player.cs
+++ Monogame Topic 9 - Making a Player Class/Player.cs
@@ -47,5 +47,42 @@ namespace Monogame_Topic_9___Making_a_Player_Class
         {
             spriteBatch.Draw(_texture, _location, Color.White);
         }
+
+        public bool Collide(Rectangle item)
+        {
+            return _location.Intersects(item);
+        }
+
+        public void Offscreen(Rectangle screenSize)
+        {
+            if (_location.Left >= screenSize.Width)
+            {
+                _location.X = (1 - _location.Width);
+            }
+            if (_location.Right <= 0)
+            {
+                _location.X = screenSize.Width;
+            }
+            if (_location.Top >= screenSize.Height)
+            {
+                _location.Y = (1 - _location.Height);
+            }
+            if (_location.Bottom <= 0)
+            {
+                _location.Y = screenSize.Height;
+            }
+        }
+
+        public void UndoMove()
+        {
+            _location.X -= (int)_speed.X;
+            _location.Y -= (int)_speed.Y;
+        }
+
+        public void Grow()
+        {
+            _location.Width += 1;
+            _location.Height += 1;
+        }
     }
 }
